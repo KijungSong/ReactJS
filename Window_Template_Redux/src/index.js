d68099f7a74bc90380980c712df856d5f43f07a7
daebaksong <skj4522@gamil.com>
2017-10-20 00:09:04 +0900

    redux 간단한 예제 추가.

--- Window_Template_Redux/src/index.js
+++ Window_Template_Redux/src/index.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './App';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      state = state + action.payload;
+      break;
+    case 'SUBTRACT':
+      state = state - action.payload;
+      break;
+  }
+  return state;
+};
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+  console.log("Store update!", store.getState())
+})
+store.dispatch({
+    type: 'ADD',
+    payload: 10
+});
+
+store.dispatch({
+    type: 'ADD',
+    payload: 30
+});
+
+store.dispatch({
+    type: 'ADD',
+    payload: 20
+});
 
 let rootElement = document.getElementById('root');
 
